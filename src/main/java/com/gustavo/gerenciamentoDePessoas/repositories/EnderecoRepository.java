a2915ceddd634ab420a5a7ef5700124494eb56ac
Gustavo Cruz <gu.cruz17@hotmail.com>
2023-04-10 21:13:41 -0300

    Criando o findByPessoa em EnderecoRepository e os testes

--- src/main/java/com/gustavo/gerenciamentoDePessoas/repositories/EnderecoRepository.java
+++ src/main/java/com/gustavo/gerenciamentoDePessoas/repositories/EnderecoRepository.java
@@ -1,9 +1,16 @@
 package com.gustavo.gerenciamentoDePessoas.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.gustavo.gerenciamentoDePessoas.entities.Endereco;
+import com.gustavo.gerenciamentoDePessoas.entities.Pessoa;
 
+@Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+	
+	List<Endereco> findByPessoa(Pessoa pessoa);
 
 }
--- src/test/java/com/gustavo/gerenciamentoDePessoas/repositories/EnderecoRepositoryTest.java
+++ src/test/java/com/gustavo/gerenciamentoDePessoas/repositories/EnderecoRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.gustavo.gerenciamentoDePessoas.repositories;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -30,11 +31,11 @@ public class EnderecoRepositoryTest {
 	@DisplayName("Deve salvar um novo Endereço")
 	public void saveEnderecoTest() {
 		// Cenário
-		Pessoa newPessoa = new Pessoa(null, "Gustavo Silva Cruz", LocalDate.of(1996, 10, 17));
+		Pessoa pessoa = new Pessoa(null, "Gustavo Silva Cruz", LocalDate.of(1996, 10, 17));
 		
-		entityManager.persist(newPessoa);
+		entityManager.persist(pessoa);
 		
-		Endereco newEndereco = new Endereco(null, "Rua Belém", "88160-396", "646", "Biguaçu", false, newPessoa);
+		Endereco newEndereco = new Endereco(null, "Rua Belém", "88160-396", "646", "Biguaçu", false, pessoa);
 		
 		// Execução
 		Endereco savedEndereco = enderecoRepository.save(newEndereco);
@@ -46,6 +47,28 @@ public class EnderecoRepositoryTest {
 		Assertions.assertThat(savedEndereco.getNumero()).isEqualTo("646");
 		Assertions.assertThat(savedEndereco.getCidade()).isEqualTo("Biguaçu");
 		Assertions.assertThat(savedEndereco.getPessoa()).isNotNull();
+	}
+	
+	@Test
+	@DisplayName("Deve obter a lista de endereços da pessoa")
+	public void findByPessoaTest() {
+		// Cenário
+		Pessoa pessoa = new Pessoa(null, "Gustavo Silva Cruz", LocalDate.of(1996, 10, 17));
+		
+		entityManager.persist(pessoa);
+		
+		Endereco endereco1 = new Endereco(null, "Rua Belém", "88160-396", "646", "Biguaçu", false, pessoa);
+		Endereco endereco2 = new Endereco(null, "Rua Oclécio Barbosa Martins", "79050-460", "341", "Campo Grande", false, null);
+		
+		entityManager.persist(endereco1);
+		entityManager.persist(endereco2);
+				
+		// Execução
+		List<Endereco> foundEnderecos = enderecoRepository.findByPessoa(pessoa);
+		
+		// Verificação
+		Assertions.assertThat(foundEnderecos).hasSize(1);
+		Assertions.assertThat(foundEnderecos).contains(endereco1);
 	}
 
 }
